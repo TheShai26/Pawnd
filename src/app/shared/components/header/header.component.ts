09402c5b31ec94bab5ff0c9d15c3eaf006adf12b
Isaí J. Farías <isaijfarias@gmail.com>
2025-05-15 16:11:08 -0400

    Base pawnd creada, en proceso la validacion de formulario de inicio de sesión

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent  implements OnInit {
+
+  @Input() title!:string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
